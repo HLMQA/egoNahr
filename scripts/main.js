8c2a1a69e0da1529417ea1ff5c333ca929439772
Houda Lamqaddam <lamqaddam.houda@gmail.cm>
2018-03-28 17:01:18 +0200

    Graph works on all tested usecases. Need to fix Y positions depending on depth in family tree

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,6 @@ var BAPTISM_LABEL = "Baptism"
 var FUNERAL_LABEL = "Funeral"
 var OFFSPRING_LABEL = "Offspring"
 
-
 //  var ActorFinal = function (id, fullName, baptismDate, birthYear, birthPlace, deathYear, gender, occupation, firstAppearance, firstParent, secondParent, spouse, firstGodParent, secondGodParent, offSpringList) {
 //     this.ID = id;
 //     this.firstName = firstName;
@@ -27,27 +26,38 @@ var OFFSPRING_LABEL = "Offspring"
 // };
 
 
-
-
-var Actor = function (id, fullName, firstParent, secondParent, spouse, offSpringList) {
-    this.ID = id;
+var Node = function (id, fullName, firstParent, secondParent, spouse, offSpringList) {
+    this.ID = "" + id;
     this.fullName = fullName;
     this.firstParent = firstParent;
     this.secondParent = secondParent;
     this.offSpringList = offSpringList;
-
     this.spouse = spouse;
+    console.log(this.ID)
+    if (this.ID.includes("+")) {
+        this.isActor = false;
+    }
+    else this.isActor = true;
 };
 
-var Tie = function (actorID1, tieType, actorID2, tieStartYear, tieEndYear) {
-    this.actorID1 = actorID1;
-    this.actorID2 = actorID2;
+var Tie = function (source, target, tieType, tieStartYear, tieEndYear) {
+    this.source = source;
+    this.target = target;
     this.tieType = tieType;
     this.tieStartYear = tieStartYear;
     this.tieEndYear = tieEndYear;
 };
 
-var actorList = [];
+var nodeList = [];
+var tieList = [];
+
+
+var simulation;
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
+
+var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 
 function getJSON(id) {
@@ -97,15 +107,19 @@ function parseJSON(id, actorJSON) {
 
     var offSpringList = getOffspringList(actorJSON);
 
-    var centralActor = new Actor(id, actorFullName, firstParent, secondParent, actorSpouse, offSpringList)
+    var centralActor = new Node(id, actorFullName, firstParent, secondParent, actorSpouse, offSpringList)
 
     if (firstParent || secondParent) {
-        createParentTies(centralActor);
+        createParentsUnionNode(centralActor);
     }
 
-    actorList.push(centralActor);
+    if (offSpringList || actorSpouse) {
+        createCentralActorUnionNode(centralActor);
+    }
 
-    console.log(actorList);
+
+    nodeList.push(centralActor);
+
 
     return myActorData;
 
@@ -119,28 +133,31 @@ function getFullNameFromJSON(json) {
         return (fullName);
     }
 }
+
 function getFirstParentFromJSON(json) {
     var element = checkLabelExistence(BAPTISM_LABEL, json);
     if (!element) return null;
-    var firstParentID = parseInt(element.data.parents.data[0]["query-value"]);
+    var firstParentID = element.data.parents.data[0]["query-value"];
     var firstParentName = element.data.parents.data[0]["query-text"];
     if (firstParentID) {
-        var thisFirstParent = new Actor(firstParentID, firstParentName);
-        actorList.push(thisFirstParent);
+        var thisFirstParent = new Node(firstParentID, firstParentName);
+        nodeList.push(thisFirstParent);
         return (thisFirstParent);
     }
 }
+
 function getSecondParentFromJSON(json) {
     var element = checkLabelExistence(BAPTISM_LABEL, json);
     if (!element) return null;
-    var secondParentID = parseInt(element.data.parents.data[2]["query-value"]);
+    var secondParentID = element.data.parents.data[2]["query-value"];
     var secondParentName = element.data.parents.data[2]["query-text"];
     if (secondParentID) {
-        var thisSecondParent = new Actor(secondParentID, secondParentName);
-        actorList.push(thisSecondParent);
+        var thisSecondParent = new Node(secondParentID, secondParentName);
+        nodeList.push(thisSecondParent);
         return (thisSecondParent);
     }
 }
+
 function getFirstGodParentFromJSON(json) {
     var element = checkLabelExistence(BAPTISM_LABEL, json);
     if (!element) return null;
@@ -149,6 +166,7 @@ function getFirstGodParentFromJSON(json) {
         return (firstGodParentID);
     }
 }
+
 function getSecondGodParentFromJSON(json) {
     var element = checkLabelExistence(BAPTISM_LABEL, json);
     if (!element) return null;
@@ -157,6 +175,7 @@ function getSecondGodParentFromJSON(json) {
         return (secondGodParentID);
     }
 }
+
 function getBaptismPlace(json) {
     var element = checkLabelExistence(BAPTISM_LABEL, json);
     if (!element) return null;
@@ -166,6 +185,7 @@ function getBaptismPlace(json) {
         return (baptismPlace);
     }
 }
+
 function getBaptismDate(json) {
     var element = checkLabelExistence(BAPTISM_LABEL, json);
     if (!element) return null;
@@ -175,6 +195,7 @@ function getBaptismDate(json) {
         return (baptismDate);
     }
 }
+
 function getBaptismSource(json) {
     var element = checkLabelExistence(BAPTISM_LABEL, json);
     if (!element) return null;
@@ -184,18 +205,20 @@ function getBaptismSource(json) {
         return (baptismSource);
     }
 }
+
 function getSpouseID(json) {
     var element = checkLabelExistence(MARRIAGE_LABEL, json);
     if (!element) return null;
-    var spouseID = parseInt(element.data[0].data.spouse.data[0]["query-value"]);
+    var spouseID = element.data[0].data.spouse.data[0]["query-value"];
     var spouseName = element.data[0].data.spouse.data[0]["query-text"];
 
     if (spouseID) {
-        var thisSpouse = new Actor(spouseID, spouseName);
-        actorList.push(thisSpouse);
+        var thisSpouse = new Node(spouseID, spouseName);
+        nodeList.push(thisSpouse);
         return (thisSpouse);
     }
 }
+
 function getFuneralDate(json) {
     var element = checkLabelExistence(FUNERAL_LABEL, json);
     if (!element) return null;
@@ -205,6 +228,7 @@ function getFuneralDate(json) {
         return (funeralDate);
     }
 }
+
 function getOffspringList(json) {
     var element = checkLabelExistence(OFFSPRING_LABEL, json);
     if (!element) return null;
@@ -216,25 +240,73 @@ function getOffspringList(json) {
         var offSpringList = [];
         for (var i = 0; i < rawOffspringArray.length; i++) {
             if (rawOffspringArray[i]["query-type"] === "actor") {
-                var thisChildID = parseInt(rawOffspringArray[i]["query-value"]);
-                var thisChild = new Actor(thisChildID);
+                var thisChildID = rawOffspringArray[i]["query-value"];
+                var thisChild = new Node(thisChildID);
                 if (rawOffspringArray[i + 2]["query-type"] === "year") {
                     var thisChildName = rawOffspringArray[i]["query-text"];
                     thisChild.fullName = thisChildName
                 }
                 offSpringList.push(thisChild);
-                actorList.push(thisChild);
+                nodeList.push(thisChild);
             }
         }
         return (offSpringList);
     }
 }
 
-function createParentTies(actor) {
-    console.log(actor)
-    return null;
+function createParentsUnionNode(actor) {
+    var firstParentToMarriageTie, secondParentToMarriageTie;
+
+    var firstParentID = relativeIDToString(actor.firstParent);
+    var secondParentID = relativeIDToString(actor.secondParent);
+
+    var parentsMarriageNode = new Node(firstParentID + "+" + secondParentID);
+    nodeList.push(parentsMarriageNode);
+
+    if (firstParentID.length > 0) {
+        firstParentToMarriageTie = new Tie(firstParentID, parentsMarriageNode.ID);
+    }
+    if (secondParentID.length > 0) {
+        secondParentToMarriageTie = new Tie(secondParentID, parentsMarriageNode.ID);
+    }
+
+    var marriageToCentralActorTie = new Tie(parentsMarriageNode.ID, actor.ID);
+
+    tieList.push(firstParentToMarriageTie, secondParentToMarriageTie, marriageToCentralActorTie);
+
 }
 
+function createCentralActorUnionNode(actor) {
+
+    var actorSpouseID = relativeIDToString(actor.spouse);
+    var actorMarriageNode = new Node(actor.ID + "+" + actorSpouseID);
+    console.log(actorMarriageNode);
+    nodeList.push(actorMarriageNode);
+    var actorToMarriageTie = new Tie(actor.ID, actorMarriageNode.ID);
+    tieList.push(actorToMarriageTie);
+
+    if (actor.spouse) {
+        var spouseToMarriageTie = new Tie(actor.spouse.ID, actorMarriageNode.ID);
+        tieList.push(spouseToMarriageTie);
+    }
+
+    if (actor.offSpringList) {
+        for (var i = 0; i < actor.offSpringList.length; i++) {
+            var marriageToOffspringTie = new Tie(actorMarriageNode.ID, actor.offSpringList[i].ID);
+            tieList.push(marriageToOffspringTie);
+        }
+
+
+    }
+}
+
+function relativeIDToString(relative) {
+    if (relative)
+        relativeID = relative.ID;
+    else relativeID = "";
+    return relativeID;
+
+}
 
 function checkLabelExistence(label, json) {
     var element = search(label, json);
@@ -244,6 +316,7 @@ function checkLabelExistence(label, json) {
         return element;
     }
 }
+
 function search(nameKey, myArray) {
     for (var i = 0; i < myArray.length; i++) {
         if (myArray[i].label === nameKey) {
@@ -251,10 +324,129 @@ function search(nameKey, myArray) {
         }
     }
 }
+
 function getActors(id) {
     var myActorJSON = getJSON(id);
     var myActorObject = parseJSON(id, myActorJSON);
 
+}
+
+function drawGraph(nodes, links) {
+
+    var graph = {};
+    graph["nodes"] = nodes;
+    graph["links"] = links;
+
+    console.log(graph);
+
+    simulation = d3.forceSimulation()
+        .force("link", d3.forceLink().id(function (d) {
+            console.log(d);
+            return "" + d.ID;
+        }))
+        .force("charge", d3.forceManyBody().strength(-250))
+        .force("center", d3.forceCenter(width / 2, height / 2));
+
+
+
+    // graph.nodes.filter(function (d) {
+    //     if (d.type === "root") {
+    //         d.fx = 100;
+    //     }
+    //     else if (d.type === "leaf") {
+    //         d.fx = 500;
+    //     }
+    //
+    // });
+
+    var link = svg.append("g")
+        .attr("class", "links")
+        .selectAll("line")
+        .data(graph.links)
+        .enter().append("line");
+
+    var node = svg.append("g")
+        .attr("class", "nodes")
+        .selectAll("g")
+        .data(graph.nodes)
+        .enter().append("g");
+
+    console.log(node);
+
+
+    var rectangle = node.append("rect")
+        .attr("width", function (d) {
+            if (d.isActor) {
+                return 30;
+            }
+            else return 0;
+        })
+        .attr("height", 6)
+        .attr("x", -15)
+        .attr("y", -3)
+        .attr("fill", function (d) {
+
+            return color(d.group);
+        })
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
+    var labels = node.append("text")
+        .text(function (d) {
+            if (d.isActor)
+                return d.fullName;
+        })
+        .attr('x', 6)
+        .attr('y', 3);
+
+
+    simulation
+        .nodes(graph.nodes)
+        .on("tick", ticked);
+
+    simulation.force("link")
+        .links(graph.links);
+
+    function ticked() {
+        link
+            .attr("x1", function (d) {
+                return d.source.x;
+            })
+            .attr("y1", function (d) {
+                return d.source.y;
+            })
+            .attr("x2", function (d) {
+                return d.target.x;
+            })
+            .attr("y2", function (d) {
+                return d.target.y;
+            });
+
+        node
+            .attr("transform", function (d) {
+                return "translate(" + d.x + "," + d.y + ")";
+            })
+    }
+}
+
+function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    // d.fx = d.x;
+    // d.fy = d.y;
+
+}
+
+function dragged(d) {
+    // d.fx = d3.event.x;
+    // d.fy = d3.event.y;
+}
+
+function dragended(d) {
+    if (!d3.event.active) simulation.alphaTarget(0);
+    // d.fx = null;
+    // d.fy = null;
 }
 
 
@@ -262,11 +454,9 @@ function getActors(id) {
 // console.log(result);
 
 // getActors(4);
-// getActors(2);
 // getActors(42);
-getActors(490);
-// getActors(480);
+getActors(480);
+// getActors("490");
+drawGraph(nodeList, tieList);
 
-
-// function drawGraph(){}
 
